66913013ed31b00f6dbc0c1076791a5c9073070f
LennartWinter <120097925+LennartWinter@users.noreply.github.com>
2024-05-22 15:47:39 +0200

    Initial commit

---
+++ resources/views/livewire/search-box.blade.php
@@ -0,0 +1,11 @@
+<div>
+    <div class="header">
+        <div class="searchbar">
+            <img src="images/searchicon.jpg" alt="logo" />
+            <input type="text" wire:model="search" placeholder="Search...">
+        </div>
+        <button wire:click="search">Add Website</button>
+    </div>
+    <div class="main">
+    </div>
+</div>
